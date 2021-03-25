00bd38afcec1e3f5fe3e3c47318fae19ddc0e197
SultanaK <kr.shampa@yahoo.com>
2021-03-24 20:55:15 -0500

    implement tour for the appliction using popover

--- growprogress-junior-product-engineer-skills-test-94cfe27f75b4/assets/onboarding.js
+++ growprogress-junior-product-engineer-skills-test-94cfe27f75b4/assets/onboarding.js
@@ -1,39 +1,74 @@
 // Write your js code here
+var $ = jQuery.noConflict();
+$(document).ready(function() {
+	$('#contact_form').on('submit', function(e) {
+		var postData = $(this).serializeArray();
+		var formURL = $(this).attr('action');
+		$.ajax({
+			url: formURL,
+			type: 'POST',
+			data: postData,
+			success: function(data, textStatus, jqXHR) {
+				$('#contact_dialog .modal-header .modal-title').html('Result');
+				$('#contact_dialog .modal-body').html(data);
+				$('#submitForm').remove();
+			},
+			error: function(jqXHR, status, error) {
+				console.log(status + ': ' + error);
+			},
+		});
+		e.preventDefault();
+	});
 
-
-$(document).ready(function () {
-    $("#contact_form").on("submit", function(e) {
-        var postData = $(this).serializeArray();
-        var formURL = $(this).attr("action");
-        $.ajax({
-            url: formURL,
-            type: "POST",
-            data: postData,
-            success: function(data, textStatus, jqXHR) {
-                $('#contact_dialog .modal-header .modal-title').html("Result");
-                $('#contact_dialog .modal-body').html(data);
-                $("#submitForm").remove();
-            },
-            error: function(jqXHR, status, error) {
-                console.log(status + ": " + error);
-            }
-        });
-        e.preventDefault();
-    });
-     
-    $("#submitForm").on('click', function() {
-        $("#contact_form").submit();
-    });
+	$('#submitForm').on('click', function() {
+		$('#contact_form').submit();
+	});
 });
 
 //driver.js Popover
 
 const driver = new Driver();
-driver.highlight({
-  element: '#sidebar-filters',
-  popover: {
-    title: 'Title for the Popover',
-    description: 'Description for it',
+ driver.defineSteps([
+  {
+    element: '#sidebar-content',
+    popover: {
+    title: 'Sidebar',
+    description: 'Select  your option',
   }
-});
-driver.start();
+    
+  },
+  {
+    element: '#widget-1',
+    popover: {
+      title: 'Favorite Ice Cream',
+      description: 'Represent Votes for Icecream',
+      position: 'top'
+    }
+  },
+  {
+    element: '#widget-2',
+    popover: {
+      title: 'Favorite Letter in Alphabet',
+      description: 'letter representation',
+      position: 'right'
+    }
+  },
+  {
+    element: '#widget-3',
+    popover: {
+      title: 'Title on Popover',
+      description: 'Body of the popover',
+      position: 'right'
+    }
+  },
+]);
+
+/* driver.highlight({
+  element: '#sidebar-content',
+  popover: {
+    title: 'Sidebar',
+    description: 'Select  your option',
+  }
+}); */
+driver.start(); 
+
